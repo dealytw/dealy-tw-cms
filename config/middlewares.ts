daa7ec22ab73ccc25cc5fd335da1fa57bafbab73
dealytw <dealytw@gmail.com>
2025-08-30 23:53:24 +0100

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,16 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:8080','https://studio.dealy.tw'], // Updated for your local port
+      methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+      headers: '*',
+      credentials: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
